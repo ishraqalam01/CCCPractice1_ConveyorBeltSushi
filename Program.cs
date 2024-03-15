40b36b6d3c39d67b12b45c6b3ce1a82a58284712
ishraqalam01 <162916694+ishraqalam01@users.noreply.github.com>
2024-03-15 12:42:08 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Name: Ishraq Alam
+//Date: March 13, 2024
+//CCC Practice Question 1 2024 Answer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable Declaration
+            string strUser;
+            int intRedPlate;
+            int intGreenPlate;
+            int intBluePlate;
+            int intTotalCost;
+
+            //Input
+            Console.WriteLine("How many red plates?");
+            strUser = Console.ReadLine();
+            intRedPlate = Int32.Parse(strUser);
+
+            Console.WriteLine("How many green plates?");
+            strUser = Console.ReadLine();
+            intGreenPlate = Int32.Parse(strUser);
+
+            Console.WriteLine("How many blue plates?");
+            strUser = Console.ReadLine();
+            intBluePlate = Int32.Parse(strUser);
+
+            //Process
+            intTotalCost = (intRedPlate * 3) + (intGreenPlate * 4) + (intBluePlate * 5);
+
+            //Output
+            Console.WriteLine("The total cost of your meals is: $" + intTotalCost);
+
+            Console.ReadKey();
+
+        }
+    }
+}
